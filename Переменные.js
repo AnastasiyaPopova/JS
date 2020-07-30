8f3eaf1b419cb1b27470e1f8c4585649612ec9e4
Anastasiya <nastyapopovaa1@gmail.com>
2020-07-31 06:35:21 +0900

    Переменные в JavaScript

---
+++ Переменные.js
@@ -0,0 +1,66 @@
+//trelloTasks
+
+//Task1
+
+'use strict';
+let num = 3;
+console.log(num);
+
+//Task2
+
+'use strict';
+let a = 10,
+    b = 2;
+alert(a + b);
+alert(a - b);
+alert(a * b);
+alert(a / b);
+
+//Task3
+
+'use strict';
+let c = 15,
+    d = 2,
+    result = c + d;
+console.log(result);
+
+
+//Task4
+
+'use strict';
+let a = 10,
+    b = 2,
+    c = 5;
+console.log(a + b + c);
+
+//Task5
+
+'use strict';
+let a = 17,
+    b = 10,
+    c = a - b,
+    d = 7,
+    result = c + d;
+console.log(result);
+
+
+///////////////////////////////////
+
+// learnJavaScriptTasks
+
+//Task1
+
+let admin;
+let name = 'Джон';
+admin = name;
+alert(admin);
+
+//Task2
+
+let ourPlanet = 'Земля';//1
+let currentUser = 'Анастасия';//2
+
+//Task3
+
+const BIRTHDAY = '18.04.1982';
+const age = someCode(BIRTHDAY);
